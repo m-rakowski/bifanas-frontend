9d40ed5b30e37dea5d707c1176c0e9b6148d4f79
m-rakowski <michalrakowski@yahoo.com>
2022-01-19 09:21:09 +0000

    code cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,106 +2,84 @@ import {useState} from "react";
 import {useUser} from '@auth0/nextjs-auth0';
 import Image from 'next/image';
 import '../styles/Index.module.scss'
-import {Box, Button, Center, Flex, Heading, HStack, Spinner, Text, useToast} from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Center,
+    Heading,
+    HStack,
+    NumberDecrementStepper,
+    NumberIncrementStepper,
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
+    Spinner,
+    Stack,
+    Text,
+    useToast
+} from "@chakra-ui/react";
 import Dropzone from "../components/dropzone";
 import axios from "axios";
 
-export interface OcrResponse {
+export interface OcrResponseRM {
     text: string;
     total: string;
+    savedName: string;
 }
+
 export default function Index() {
     const [requestInProgress, setRequestInProgress] = useState<boolean>(false);
-    const [image, setImage] = useState(null);
-    const [data, setData] = useState<OcrResponse>(null);
-    const [secureUrl, setSecureUrl] = useState(null);
-    const [createObjectURL, setCreateObjectURL] = useState(null);
+    const [data, setData] = useState<OcrResponseRM>({total: '', text: '', savedName: ''});
+    const [uploadedImage, setUploadedImage] = useState(null);
+    const [inputValue, setInputValue] = useState("");
     const toast = useToast()
 
     const uploadToClient = async (image) => {
-        console.log(image.abc);
         if (image) {
-            setImage(image);
-            setCreateObjectURL(URL.createObjectURL(image));
+            setUploadedImage(URL.createObjectURL(image));
         }
 
-        setData(await uploadStraightToOCRServer(image));
+        const ocrResponseRM = await uploadFileToBackend(image);
+        setData(ocrResponseRM);
+        setInputValue(ocrResponseRM?.total);
     };
 
-    const uploadToServer = async (image) => {
+    const updateTotal = async () => {
         try {
-            setRequestInProgress(true);
-            const formData = new FormData();
-            formData.append("file", image);
-            const response = await axios.post(
-                "/api/file",
-                formData, {
-                    headers: {
-                        'Content-Type': 'multipart/form-data'
-                    }
-                });
-            setSecureUrl(response.data.secure_url)
+            await axios.put<OcrResponseRM>(
+                "/backend/api/image/update-total",
+                {total: inputValue, savedName: data.savedName}, {});
             toast({
                 title: `File uploaded`,
                 status: 'success',
                 isClosable: true,
             });
-            return response.data.secure_url;
-
         } catch (err) {
             toast({
                 title: err?.message,
                 status: 'error',
                 isClosable: true,
             });
-        } finally {
-            setRequestInProgress(false);
         }
     };
-    const uploadStraightToOCRServer = async (image) => {
+
+
+    const uploadFileToBackend = async (image): Promise<OcrResponseRM> => {
         try {
             setRequestInProgress(true);
             const formData = new FormData();
             formData.append("file", image);
 
-            const response = await axios.post<OcrResponse>(
+            const response = await axios.post<OcrResponseRM>(
                 "/backend/api/image/ocr",
                 formData, {
                     headers: {
                         'Content-Type': 'multipart/form-data'
                     }
                 });
-            toast({
-                title: `File uploaded`,
-                status: 'success',
-                isClosable: true,
-            });
 
             return response.data;
 
-        } catch (err) {
-            toast({
-                title: err?.message,
-                status: 'error',
-                isClosable: true,
-            });
-        } finally {
-            setRequestInProgress(false);
-        }
-    };
-    const getOCR = async (secure_url) => {
-        try {
-            setRequestInProgress(true);
-            const res = await axios.post("/api/receipt",
-                {secure_url},
-            );
-            setData(res.data);
-
-            toast({
-                title: `Scanning for text`,
-                status: 'success',
-                isClosable: true,
-            });
         } catch (err) {
             toast({
                 title: err?.message,
@@ -125,16 +103,35 @@ export default function Index() {
                     color='blue.500'
                     size='xl'
                 />}
-                <Heading as='h3' size='lg'>Amount: {data?.total}</Heading>
-                <Dropzone onFileAccepted={uploadToClient}/>
+                <Stack>
+                    <Dropzone onFileAccepted={uploadToClient}/>
+                    {uploadedImage && data && data.savedName && <HStack>
+                        <Heading as='h3' size='lg'>Amount:</Heading>
+                        <NumberInput
+                            onChange={() => setInputValue(event.target?.value)}
+                            value={inputValue}
+                            precision={2} step={0.1} width={'300px'}
+                        >
+                            <NumberInputField
 
+                            />
+                            <NumberInputStepper>
+                                <NumberIncrementStepper/>
+                                <NumberDecrementStepper/>
+                            </NumberInputStepper>
+                        </NumberInput>
+                        <Button colorScheme='teal' size='md' onClick={updateTotal}>
+                            Send
+                        </Button>
+                    </HStack>}
+                </Stack>
                 <Center>
                     <Box p={4} display={{md: 'flex'}}>
                         <Box flexShrink={0}>
-                            {createObjectURL && <Image src={createObjectURL}
-                                                       alt={'uploaded image'}
-                                                       width={400}
-                                                       height={800}
+                            {uploadedImage && <Image src={uploadedImage}
+                                                     alt={'uploaded image'}
+                                                     width={400}
+                                                     height={800}
                             />}
                         </Box>
                         <Box mt={{base: 4, md: 0}} ml={{md: 6}}>
